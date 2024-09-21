4811a782187964e4fe44cc6a6e550e6219f784e8
Nmn <Naman.tortalabz@hotmail.com>
2024-09-21 18:13:53 +0530

    app navigation added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,84 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { colors, fontSize } from '@/constants/tokens'
+import { FontAwesome, FontAwesome6, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+// import { BlurView } from 'expo-blur'
+import { Tabs } from 'expo-router'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const TabsNavigation = () => {
+	return (
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: colors.primary,
+					tabBarLabelStyle: {
+						fontSize: fontSize.xs,
+						fontWeight: '500',
+					},
+					headerShown: false,
+					tabBarStyle: {
+						position: 'absolute',
+						borderTopLeftRadius: 20,
+						borderTopRightRadius: 20,
+						borderTopWidth: 0,
+						paddingTop: 8,
+					},
+					// tabBarBackground: () => (
+					// 	<BlurView
+					// 		intensity={95}
+					// 		style={{
+					// 			...StyleSheet.absoluteFillObject,
+					// 			overflow: 'hidden',
+					// 			borderTopLeftRadius: 20,
+					// 			borderTopRightRadius: 20,
+					// 		}}
+					// 	/>
+					// ),
+				}}
+			>
+				<Tabs.Screen
+					name="(songs)"
+					options={{
+						title: 'Songs',
+						tabBarIcon: ({ color }) => (
+							<Ionicons name="musical-notes-sharp" size={24} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="favorites"
+					options={{
+						title: 'Favorites',
+						tabBarIcon: ({ color }) => <FontAwesome name="heart" size={20} color={color} />,
+					}}
+				/>
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+				<Tabs.Screen
+					name="playlists"
+					options={{
+						title: 'Playlists',
+						tabBarIcon: ({ color }) => (
+							<MaterialCommunityIcons name="playlist-play" size={28} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="artists"
+					options={{
+						title: 'Artists',
+						tabBarIcon: ({ color }) => <FontAwesome6 name="users-line" size={20} color={color} />,
+					}}
+				/>
+			</Tabs>
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+			{/* <FloatingPlayer
+				style={{
+					position: 'absolute',
+					left: 8,
+					right: 8,
+					bottom: 78,
+				}}
+			/> */}
+		</>
+	)
 }
+
+export default TabsNavigation
